5b2cb4a50c3a78950a3f8a9870f2c5871b36cd81
colocky <callmeadam.ar@gmail.com>
2026-04-10 14:19:09 -0500

    Added the loaded option and added price modifications to it.

---
+++ src/main/java/pluralsight/SandwichShop.java
@@ -0,0 +1,63 @@
+package pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // stablishing sandwich size
+        double sandwichPrice = 0.00;
+        System.out.print("What sandwich size do you want? (1 or 2)\n" +
+                "1: Regular: base price $5.45\n" +
+                "2: Large: base price $8.95\n");
+        int sandwichSize = scanner.nextInt();
+        scanner.nextLine();
+
+        // stablishing sandwich price
+        if (sandwichSize == 1) {
+            sandwichPrice = 5.45;
+        } else if (sandwichSize == 2) {
+            sandwichPrice = 8.95;
+        } else {
+            System.out.println("Invalid size try again.");
+            return;
+        }
+
+        // stablishing if sandwich is loaded
+        boolean loaded = false;
+        double loadedPrice = 0;
+        System.out.println("Would you like your sandwich loaded? (Y/N");
+        String sandwichLoaded = scanner.nextLine();
+        if (sandwichLoaded.equals("Y")) {
+            loaded = true;
+        } else if (sandwichLoaded.equals("N")) {
+            loaded = false;
+        } else {
+            System.out.println("Invalid choice try again");
+            return;
+        }
+
+        // stablishing loaded price
+        if (loaded) {
+            if (sandwichSize == 1) {
+                loadedPrice = 1.00;
+            } else {
+                loadedPrice = 1.75;
+            }
+        }
+
+        // stablishing age
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        // final result
+        if (age <= 17) {
+            System.out.println("Your sandwich will be: $" + String.format("%.2f", (sandwichPrice + loadedPrice) * 0.90));
+        } else if (age >= 65) {
+            System.out.println("Your sandwich will be: $" + String.format("%.2f", (sandwichPrice + loadedPrice) * 0.80));
+        } else {
+            System.out.println("Your sandwich will be: $" + String.format("%.2f", (sandwichPrice + loadedPrice)));
+        }
+    }
+}
